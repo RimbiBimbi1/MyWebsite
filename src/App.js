df215cd5a458301eb52015a9ff450d42e8250358
RimbiBimbi1 <103674299+RimbiBimbi1@users.noreply.github.com>
2023-05-14 19:44:56 +0200

    File structure v0.1.1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from './components/Layout/Layout';
+import { Home } from './pages/Home/Home';
 
 function App() {
   return (
-    <></>
+    <Routes>
+      <Route path={'/*'} element={<Layout />}>
+        <Route path={'*'} element={<Home />}></Route>
+      </Route>
+    </Routes>
   );
 }
 
