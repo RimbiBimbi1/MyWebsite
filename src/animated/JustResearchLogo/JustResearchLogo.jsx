6f5dd70f3c249a8a43ee85ff4bd619da6d0c69ca
RimbiBimbi1 <103674299+RimbiBimbi1@users.noreply.github.com>
2023-06-12 22:38:51 +0200

    code-cleaning-3

--- src/animated/JustResearchLogo/JustResearchLogo.jsx
+++ src/animated/JustResearchLogo/JustResearchLogo.jsx
@@ -4,7 +4,7 @@ import logo from './logo512.png';
 export const JustResearchLogo = () => {
   return (
     <div className={styles.container}>
-      <a href="https://github.com/UMK-PZ2022-Zesp01" target={'_blank'}>
+      <a href="https://github.com/UMK-PZ2022-Zesp01" target={'_blank'} rel={'noreferrer'}>
         <img src={logo} alt="logo JustResearch" />
       </a>
     </div>
